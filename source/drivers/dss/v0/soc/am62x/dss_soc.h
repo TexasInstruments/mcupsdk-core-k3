2dc5786ba820dcdb801699865824d0d5ebd2adbe
zeba <x1120004@ti.com>
2023-12-12 13:36:18 +0530

    am62x: dss: fix MISRA.FUNC.NO_PARAMS
    Explicitly mention void for the functions
    that do not have input arg.
    
    Fixes: SITSW-3467
    
    Signed-off-by: Zeba.Aleem <zeba.aleem@ti.com>

--- source/drivers/dss/v0/soc/am62x/dss_soc.h
+++ source/drivers/dss/v0/soc/am62x/dss_soc.h
@@ -321,7 +321,7 @@ static inline void Dss_dctrlOldiParamsInit(Dss_DctrlOldiParams *oldiParams);
  *  \brief Perform DSS Soft Reset
  *
  */
-void Dss_setDssSoftReset();
+void Dss_setDssSoftReset(void);
 
 /**
  *  \brief Set OLDI Power Domain Control to power on and off OLDI TX

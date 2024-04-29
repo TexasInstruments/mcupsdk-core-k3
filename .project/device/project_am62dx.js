a035de402a18c83dc7a09c5d3970bdf9dbe8cf10
Anoop <anoop.rk@ti.com>
2024-04-29 16:38:31 +0530

    am62d: DM: Revert commit for hooks in am62ax DM
        Revert "am62dx: infra: Add hooks to generate reused am62a libs"
    
        This reverts commit 6976ebce61939466b033e0e32ecec87bc2cbc109.
    
    Fixes: SITSW-4253
    
    Signed-off-by: Anoop <anoop.rk@ti.com>

--- .project/device/project_am62dx.js
+++ .project/device/project_am62dx.js
@@ -3,6 +3,14 @@ const common = require("../common.js");
 const component_file_list = [
     "source/board/.project/project.js",
     "source/drivers/.project/project.js",
+    /* TODO: Add when DM adds AM62D support */
+    /*
+    "source/drivers/device_manager/self_reset/.project/project.js",
+    "source/drivers/device_manager/sciclient_direct/.project/project.js",
+    "source/drivers/device_manager/sciclient_direct/sbl/.project/project.js",
+    "source/drivers/device_manager/sciserver/.project/project.js",
+    "source/drivers/dmautils/.project/project.js",
+    */
     "source/dhrystone_benchmark/.project/project.js",
     "source/drivers/udma/.project/project.js",
     "source/fs/freertos_fat/.project/project.js",
@@ -11,18 +19,14 @@ const component_file_list = [
     "test/unity/.project/project.js",
 ];
 
-const reuse_component_make_list = [
-    "source/drivers/device_manager/self_reset/.project/project.js",
-    "source/drivers/device_manager/sciclient_direct/.project/project.js",
-    "source/drivers/device_manager/sciclient_direct/sbl/.project/project.js",
-    "source/drivers/device_manager/sciserver/.project/project.js",
-];
-
 // List of components where makefile is not generated.
 const component_file_list_with_makefile = [
+    /* TODO: Add when DM adds AM62D support */
+    /*
     "source/drivers/device_manager/dm_stub/.project/project.js",
     "source/drivers/device_manager/rm_pm_hal/.project/project.js",
     "source/drivers/device_manager/rm_pm_hal/sbl/.project/project.js",
+    */
 ];
 
 const device_defines = {
@@ -110,10 +114,6 @@ function getComponentList() {
     return component_file_list;
 }
 
-function getReuseComponentList() {
-    return reuse_component_make_list;
-}
-
 function getComponentListWithMakefile() {
     return component_file_list_with_makefile;
 }
@@ -181,7 +181,6 @@ function getFlashAddr() {
 
 module.exports = {
     getComponentList,
-    getReuseComponentList,
     getComponentListWithMakefile,
     getExampleList,
     getSysCfgDevice,
--- .project/genComponentFiles.js
+++ .project/genComponentFiles.js
@@ -3,45 +3,6 @@ const _ = require('lodash');
 
 function genComponentFilesDevice(device) {
 
-    if(device == "am62dx")
-    {
-        let reuse_file_list = require(`./device/project_${device}`).getReuseComponentList();
-
-        for(component of reuse_file_list) {
-            property = require(`../${component}`).getComponentProperty("am62ax");
-            for(buildOption of property.buildOptionCombos) {
-                let commonCgtOptions = require(`./cgt/cgt_${buildOption.cgt}`).getCgtOptions(buildOption.cpu);
-                let common_build_property = require(`./device/project_am62ax`).getProperty();
-                let project = [];
-
-                buildOption.device = "am62ax";
-                build_property = require(`../${component}`).getComponentBuildProperty(buildOption);
-
-                project = _.merge({}, project, property);
-                project = _.merge({}, project, buildOption);
-                project = common.mergeCgtOptions(project, common_build_property);
-                project = _.merge({}, project, build_property);
-
-                if(!project.templates)
-                    continue;
-
-                for(template of project.templates)
-                {
-                    let args = {
-                        sdkPath: "MCU_PLUS_SDK_PATH",
-                        relPath: common.path.relative(project.dirPath, "."),
-                        project: project,
-                        options: template.options,
-                    };
-
-                    common.convertTemplateToFile(
-                            template.input,
-                            `${project.dirPath}/${template.output}`,
-                            args);
-                }
-            }
-        }
-    }
     let file_list = require(`./device/project_${device}`).getComponentList();
 
     for(component of file_list) {

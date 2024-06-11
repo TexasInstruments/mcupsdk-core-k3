507377ad2649972d3fc46394c83ced27f8983d3f
Anoop <anoop.rk@ti.com>
2024-06-11 10:22:22 +0530

    am62dx: core_id: Update coreId to match 62ax
        * Update Core Id to match with 62a core Id
    
    Fixes: SITSW-4621
    
    Signed-off-by: Anoop <anoop.rk@ti.com>

--- source/drivers/bootloader/soc/am62dx/bootloader_soc.c
+++ source/drivers/bootloader/soc/am62dx/bootloader_soc.c
@@ -120,6 +120,14 @@ Bootloader_CoreBootInfo gCoreBootInfo[] =
     },
 
     {
+        .tisciProcId    = SCICLIENT_PROC_ID_C7X256V0_C7XV_CORE_0,
+        .tisciDevId     = TISCI_DEV_C7X256V0_C7XV_CORE_0,
+        .tisciClockId   = TISCI_DEV_C7X256V0_C7XV_CORE_0_C7XV_CLK,
+        .defaultClockHz = (uint32_t)(1000*1000000),
+        .coreName       = "c7x0-0",
+    },
+
+    {
         .tisciProcId    = SCICLIENT_PROC_ID_A53SS0_CORE_0,
         .tisciDevId     = TISCI_DEV_A53SS0_CORE_0,
         .tisciClockId   = TISCI_DEV_A53SS0_CORE_0_A53_CORE0_ARM_CLK_CLK,
@@ -152,20 +160,13 @@ Bootloader_CoreBootInfo gCoreBootInfo[] =
     },
 
     {
-    .tisciProcId    = SCICLIENT_PROC_ID_HSM_M4FSS0_CORE0,
-    .tisciDevId     = TISCI_DEV_HSM0,
-    .tisciClockId   = TISCI_DEV_HSM0_DAP_CLK,
-    .defaultClockHz = (uint32_t)(400*1000000),
-    .coreName       = "m4f1-0",
+        .tisciProcId    = SCICLIENT_PROC_ID_HSM_M4FSS0_CORE0,
+        .tisciDevId     = TISCI_DEV_HSM0,
+        .tisciClockId   = TISCI_DEV_HSM0_DAP_CLK,
+        .defaultClockHz = (uint32_t)(400*1000000),
+        .coreName       = "m4f1-0",
     },
 
-    {
-    .tisciProcId    = SCICLIENT_PROC_ID_C7X256V0_C7XV_CORE_0,
-    .tisciDevId     = TISCI_DEV_C7X256V0_C7XV_CORE_0,
-    .tisciClockId   = TISCI_DEV_C7X256V0_C7XV_CORE_0_C7XV_CLK,
-    .defaultClockHz = (uint32_t)(1000*1000000),
-    .coreName       = "c7x0-0",
-    },
 };
 
 Bootloader_CoreAddrTranslateInfo gAddrTranslateInfo[] =
@@ -206,6 +207,24 @@ Bootloader_CoreAddrTranslateInfo gAddrTranslateInfo[] =
         },
     },
 
+    /* CSL_CORE_ID_C75SS0_0 */
+    {
+        .numRegions = 2,
+        .addrRegionInfo =
+        {
+            {
+                .cpuLocalAddr = CSL_C7X256V0_UMC_MEM_MAIN_BASE,
+                .socAddr      = CSL_C7X256V0_UMC_MEM_MAIN_BASE,
+                .regionSize   = CSL_C7X256V0_UMC_MEM_MAIN_SIZE,
+            },
+            {
+                .cpuLocalAddr = CSL_C7X256V0_UMC_MEM_AUX_BASE,
+                .socAddr      = CSL_C7X256V0_UMC_MEM_AUX_BASE,
+                .regionSize   = CSL_C7X256V0_UMC_MEM_AUX_SIZE,
+            },
+        },
+    },
+
     /* CSL_CORE_ID_A53SS0_0 */
     {
         .numRegions = 0,
@@ -241,23 +260,6 @@ Bootloader_CoreAddrTranslateInfo gAddrTranslateInfo[] =
         },
     },
 
-    /* CSL_CORE_ID_C75SS0_0 */
-    {
-        .numRegions = 2,
-        .addrRegionInfo =
-        {
-            {
-                .cpuLocalAddr = CSL_C7X256V0_UMC_MEM_MAIN_BASE,
-                .socAddr      = CSL_C7X256V0_UMC_MEM_MAIN_BASE,
-                .regionSize   = CSL_C7X256V0_UMC_MEM_MAIN_SIZE,
-            },
-            {
-                .cpuLocalAddr = CSL_C7X256V0_UMC_MEM_AUX_BASE,
-                .socAddr      = CSL_C7X256V0_UMC_MEM_AUX_BASE,
-                .regionSize   = CSL_C7X256V0_UMC_MEM_AUX_SIZE,
-            },
-        },
-    },
 };
 
 Bootloader_SelfCoreJump selfcoreEntry = NULL;
@@ -321,7 +323,7 @@ uint32_t Bootloader_socRprcToCslCoreId(uint32_t rprcCoreId)
 
     uint32_t rprcCoreIds[CSL_CORE_ID_MAX] =
     {
-        5U, 4U, 0U, 1U, 2U, 3U, 6U, 7U
+        5U, 4U, 7U, 0U, 1U, 2U, 3U, 6U
     };
 
     if(Bootloader_socIsSmpEnable( rprcCoreId) == true)

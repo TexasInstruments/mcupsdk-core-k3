bd692fe1ff9779e8acf389586bbbb007611116f5
Anoop <anoop.rk@ti.com>
2024-06-20 18:34:15 +0530

    am62dx: mcasp: Update mcasp 8 channel playback example
        * Update mcasp 8 channel playback example for the
          board configuration.
        * Remove ppc3 files.
    
    Fixes: SITSW-4659
    
    Signed-off-by: Anoop <anoop.rk@ti.com>

--- examples/drivers/mcasp/mcasp_multichannel_playback/am62dx-evm/board.c
+++ examples/drivers/mcasp/mcasp_multichannel_playback/am62dx-evm/board.c
@@ -34,7 +34,6 @@
 /*                             Include Files                                  */
 /* ========================================================================== */
 
-#include <board/ioexp/ioexp_tca6424.h>
 #include <kernel/dpl/ClockP.h>
 #include "ti_drivers_open_close.h"
 
@@ -56,7 +55,7 @@ typedef struct {
 /*                            Global Variables                                */
 /* ========================================================================== */
 
-Dac_RegCfg gPcm6240Reg[] =
+Adc_RegCfg gPcm6240RegInst0[] =
 {
     /* Page select 0 */
     {0x00, 0x00},
@@ -65,58 +64,97 @@ Dac_RegCfg gPcm6240Reg[] =
     /* Page select 0 */
     {0x00, 0x00},
     /* Disable sleep */
-    {0x02, 0x81},
+    {0x02, 0x09},
+    /* High impedance for unused cycles */
+    {0x07, 0x31},
     /* 1 BCLK delay between FSYNC and data */
     {0x08, 0x01},
-    /* Set BCLK/FSYNC ratio. 48KHz */
-    {0x14, 0x46},
-    /* GPIO1 config */
-    {0x21, 0x02},
-    /* GPIO2 config */
-    {0x22, 0x02},
-    /* GPIO3 config */
-    {0x23, 0x02},
+    /* CH1 at slot 0 */
+    {0x0B, 0x00},
+    /* CH2 at slot 4 */
+    {0x0C, 0x04},
+    /* CH3 at slot 1 */
+    {0x0D, 0x01},
+    /* CH4 at slot 5 */
+    {0x0E, 0x05},
     /* Set micbias to 5v */
     {0x3b, 0x70},
     /* CH1 Microphone input */
-    {0x3c, 0x80},
+    {0x3c, 0x10},
     /* CH2 Microphone input */
-    {0x41, 0x80},
+    {0x41, 0x10},
     /* CH3 Microphone input */
-    {0x46, 0x80},
+    {0x46, 0x10},
     /* CH4 Microphone input */
-    {0x4b, 0x80},
+    {0x4b, 0x10},
     /* Input channel enable */
-    {0x74, 0xf0},
+    {0x74, 0xF0},
     /* Power up micbias and power up all ADC channels */
-    {0x75, 0xc0},
+    {0x75, 0xE0},
 };
 
-Adc_RegCfg gTad5212Reg[] =
+Adc_RegCfg gPcm6240RegInst1[] =
 {
     /* Page select 0 */
     {0x00, 0x00},
     /* Reset */
-    {0x00, 0x01},
-    {0x7F, 0x00},
-    /* Disable sleep mode */
+    {0x01, 0x01},
+    /* Page select 0 */
+    {0x00, 0x00},
+    /* Disable sleep */
+    {0x02, 0x09},
+    /* High impedance for unused cycles */
+    {0x07, 0x31},
+    /* 1 BCLK delay between FSYNC and data */
+    {0x08, 0x01},
+    /* CH1 at slot 2 */
+    {0x0B, 0x02},
+    /* CH2 at slot 6 */
+    {0x0C, 0x06},
+    /* CH3 at slot 3 */
+    {0x0D, 0x03},
+    /* CH4 at slot 7 */
+    {0x0E, 0x07},
+    /* Set micbias to 5v */
+    {0x3b, 0x70},
+    /* CH1 Microphone input */
+    {0x3c, 0x10},
+    /* CH2 Microphone input */
+    {0x41, 0x10},
+    /* CH3 Microphone input */
+    {0x46, 0x10},
+    /* CH4 Microphone input */
+    {0x4b, 0x10},
+    /* Input channel enable */
+    {0x74, 0xF0},
+    /* Power up micbias and power up all ADC channels */
+    {0x75, 0xE0},
+};
+
+Dac_RegCfg gTad5212Reg[] =
+{
+    /* Page select 0 */
+    {0x00, 0x00},
+    /* Disable sleep */
     {0x02, 0x01},
     /* I2S, 32b mode */
     {0x1A, 0x70},
     /* 1b delay between FSYNC and data */
     {0x26, 0x01},
-    /* CH1 - Input from DAC, Mono single ended output at OUT1M */
+    /* CH1 - Input from DAC, Mono single ended output at OUT1P */
     {0x64, 0x28},
     /* CH1 - Headphone with 4ohm impedence, 0dB gain */
     {0x65, 0x60},
-    /* CH1 - +27dB gain */
-    {0x67, 0xFF},
-    /* CH2 - Input from DAC, Mono single ended output at OUT1M */
+    /* CH1 - 0dB gain */
+    {0x67, 0xC9},
+    /* CH2 - Input from DAC, Mono single ended output at OUT2P */
     {0x6B, 0x28},
     /* CH2 - Headphone with 4ohm impedence, 0dB gain */
     {0x6C, 0x60},
-    /* CH2 - +27dB gain */
-    {0x6E, 0xFF},
+    /* CH2 - 0dB gain */
+    {0x6E, 0xC9},
+    /* PASI channel2 input is right slot0 */
+    {0x29, 0x30},
     /* Enable output CH1 CH2 */
     {0x76, 0x0C},
     /* Page select 1 */
@@ -142,6 +180,10 @@ Adc_RegCfg gTad5212Reg[] =
     {0x3e, 0x09},
     {0x48, 0x01},
     {0x49, 0x01},
+    /* Page Select 0 */
+    {0x00, 0x00},
+    {0x78, 0x40},
+
 };
 
 /* ========================================================================== */
@@ -149,7 +191,7 @@ Adc_RegCfg gTad5212Reg[] =
 /* ========================================================================== */
 
 /* Configure PCM6240 */
-int32_t Board_adcConfig(I2C_Handle handle, uint8_t devAddr);
+int32_t Board_adcConfig(I2C_Handle handle, uint8_t devAddr, uint32_t instNum);
 /* Configure TAD5212 */
 int32_t Board_dacConfig(I2C_Handle handle, uint8_t devAddr);
 
@@ -160,9 +202,7 @@ int32_t Board_dacConfig(I2C_Handle handle, uint8_t devAddr);
 int32_t Board_codecConfig(void)
 {
     int32_t status = SystemP_SUCCESS;
-    TCA6424_Config TCA6424_IOexp_config;
 
-#if 0
     /* Configure TAD5212 instances */
     {
         I2C_Handle      i2cHandle;
@@ -186,24 +226,12 @@ int32_t Board_codecConfig(void)
         I2C_Handle      i2cHandle;
         i2cHandle = gI2cHandle[CONFIG_I2C0];
 
-        status = Board_adcConfig(i2cHandle, 0x90);
+        status = Board_adcConfig(i2cHandle, 0x48, 0);
         DebugP_assert(status == SystemP_SUCCESS);
 
-        status = Board_adcConfig(i2cHandle, 0x92);
+        status = Board_adcConfig(i2cHandle, 0x49, 1);
         DebugP_assert(status == SystemP_SUCCESS);
     }
-#endif
-
-    /* Route mcasp signals to header */
-    TCA6424_Params  tca6424Params;
-    TCA6424_Params_init(&tca6424Params);
-    tca6424Params.i2cInstance = 0;
-    tca6424Params.i2cAddress = 0x22;
-    status = TCA6424_open(&TCA6424_IOexp_config, &tca6424Params);
-
-    status += TCA6424_config(&TCA6424_IOexp_config, 21, TCA6424_MODE_OUTPUT);
-
-    status += TCA6424_setOutput(&TCA6424_IOexp_config, 21, TCA6424_OUT_STATE_LOW);
 
     return status;
 }
@@ -218,6 +246,8 @@ int32_t Board_dacConfig(I2C_Handle handle, uint8_t devAddr)
 
     status = I2C_probe(handle, devAddr);
     DebugP_assert(status == SystemP_SUCCESS);
+
+    ClockP_usleep(100);
 
     for(i = 0; i < count; i++)
     {
@@ -234,16 +264,16 @@ int32_t Board_dacConfig(I2C_Handle handle, uint8_t devAddr)
             break;
         }
 
-        ClockP_usleep(10);
+        ClockP_usleep(100);
     }
 
     return status;
 }
 
-int32_t Board_adcConfig(I2C_Handle handle, uint8_t devAddr)
+int32_t Board_adcConfig(I2C_Handle handle, uint8_t devAddr, uint32_t inst)
 {
     int32_t status = SystemP_SUCCESS;
-    uint32_t count = sizeof(gPcm6240Reg)/sizeof(Dac_RegCfg);
+    uint32_t count = 0;
     uint32_t i = 0;
     I2C_Transaction i2cTransaction;
     uint8_t txBuffer[2];
@@ -251,14 +281,34 @@ int32_t Board_adcConfig(I2C_Handle handle, uint8_t devAddr)
     status = I2C_probe(handle, devAddr);
     DebugP_assert(status == SystemP_SUCCESS);
 
+    ClockP_usleep(200);
+
+    if(inst == 0)
+    {
+        count = sizeof(gPcm6240RegInst0)/sizeof(gPcm6240RegInst0[0]);
+    }
+    else if(inst == 1)
+    {
+        count = sizeof(gPcm6240RegInst1)/sizeof(gPcm6240RegInst1[0]);
+    }
+
     for(i = 0; i < count; i++)
     {
         I2C_Transaction_init(&i2cTransaction);
         i2cTransaction.writeBuf   = txBuffer;
         i2cTransaction.writeCount = 2;
         i2cTransaction.targetAddress = devAddr;
-        txBuffer[0] = gPcm6240Reg[i].reg;
-        txBuffer[1] = gPcm6240Reg[i].val;
+        if(inst == 0)
+        {
+            txBuffer[0] = gPcm6240RegInst0[i].reg;
+            txBuffer[1] = gPcm6240RegInst0[i].val;
+        }
+        else if(inst == 1)
+        {
+            txBuffer[0] = gPcm6240RegInst1[i].reg;
+            txBuffer[1] = gPcm6240RegInst1[i].val;
+        }
+
         status = I2C_transfer(handle, &i2cTransaction);
 
         if(status != SystemP_SUCCESS)
@@ -266,7 +316,7 @@ int32_t Board_adcConfig(I2C_Handle handle, uint8_t devAddr)
             break;
         }
 
-        ClockP_usleep(10);
+        ClockP_usleep(200);
     }
 
     return status;

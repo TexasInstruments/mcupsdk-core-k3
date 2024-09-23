7531e9fd3bc312dde3ff17e66565441fd080afe9
Jonathan Blanchard <jonathan.blanchard@jblopen.com>
2024-09-23 18:22:11 -0400

    eclipse_threadx: Increase TaskP object storage size for ThreadX' TX_THREAD structure.

--- source/kernel/dpl/TaskP.h
+++ source/kernel/dpl/TaskP.h
@@ -85,7 +85,7 @@ typedef struct {
 /**
  * \brief Max size of task object across all OS's
  */
-#define TaskP_OBJECT_SIZE_MAX       (176U)
+#define TaskP_OBJECT_SIZE_MAX       (200U)
 /**
  * \brief Opaque task object used with the task APIs
  */

ce9482f632e8b1ebd2835274963793d0aa8f9023
Nikhil M Jain <n-jain1@ti.com>
2024-10-03 14:27:31 +0530

    am62d: ospi: Add ospi flash diag example
    - Add build files for ospi_flash_diag freertos example for am62dx.
    - Add the example in the project_soc.js file, to enable building the
      example.
    
    Fixes: SITSW-5599
    
    Signed-off-by: Nikhil M Jain <n-jain1@ti.com>

--- .project/device/project_am62dx.js
+++ .project/device/project_am62dx.js
@@ -88,6 +88,7 @@ const example_file_list = [
     "examples/drivers/i2c/i2c_temperature/.project/project.js",
 	"examples/drivers/ipc/ipc_notify_echo/.project/project.js",
 	"examples/drivers/ipc/ipc_rpmsg_echo/.project/project.js",
+    "examples/drivers/ospi/ospi_flash_diag/.project/project.js",
 	"examples/drivers/ospi/ospi_flash_io/.project/project.js",
     "examples/drivers/sciclient/sciclient_get_version/.project/project.js",
     "examples/drivers/spinlock/spinlock_example/.project/project.js",

c69fd6bdfc2bdc6a802e9dacbcce41ce99df9e41
Nithyaa shri R B <n-rb@ti.com>
2024-10-14 16:02:22 +0530

    am62l: fs: Add FAT support
    - Add freertos_fat support for a53 core
    
    - Add syscfg file for filesystem
    
    - Add generated makefiles
    
    Fixes: SITSW-5663
    
    Signed-off-by: Nithyaa shri R B <n-rb@ti.com>

---
+++ source/fs/.meta/soc/fs_am62lx.syscfg.js
@@ -0,0 +1,12 @@
+
+let common = system.getScript("/common");
+
+const topModules_a53 = [
+    "/fs/freertos_fat/freertos_fat",
+]
+
+exports = {
+    getTopModules: function() {
+        return topModules_a53;
+    }
+}

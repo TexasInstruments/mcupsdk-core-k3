c032174df769c96319083984e0def07b1995d551
Yash Srivastava <y-srivastava@ti.com>
2025-01-08 13:21:35 +0530

    am62lx: psci: Add PSCI driver support.
    - Add initial PSCI driver support for AM62L.
    - Add get_version, cpu_off, cpu_on, system_reset and features services.
    - Update the build infra for PSCI driver.
    
    Fixes: SITSW-6285
    
    Signed-off-by: Yash Srivastava <y-srivastava@ti.com>

---
+++ source/drivers/scp/psci/v0/psci.c
@@ -0,0 +1,379 @@
+/*
+ *  Copyright (C) 2025 Texas Instruments Incorporated
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *    Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ *    Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the
+ *    distribution.
+ *
+ *    Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/* *
+ *  \file   psci.c
+ *
+ *  \brief  File containing PSCI Driver APIs implementation for V0.
+ */
+
+/* ========================================================================== */
+/*                             Include Files                                  */
+/* ========================================================================== */
+
+#include <kernel/dpl/SystemP.h>
+#include <drivers/psci.h>
+#include <drivers/scp/smccc/smccc.h>
+
+/* ========================================================================== */
+/*                           Macros & Typedefs                                */
+/* ========================================================================== */
+
+/* None */
+
+/* ========================================================================== */
+/*                         Structure Declarations                             */
+/* ========================================================================== */
+typedef struct
+{
+    void                   *lock;
+    /* *< Driver lock - to protect across open/close */
+    SemaphoreP_Object       lockObj;
+    /* *< Driver lock object */
+} PSCI_DrvObj;
+
+/* ========================================================================== */
+/*                      Internal Function Declarations                        */
+/* ========================================================================== */
+
+static inline uint32_t PSCI_function(uint32_t functionId, uint32_t arg0, uint32_t arg1, uint32_t arg2);
+static inline int32_t PSCI_handleErr(int32_t errNo);
+
+/* ========================================================================== */
+/*                            Global Variables                                */
+/* ========================================================================== */
+
+/* Externs */
+extern PSCI_Config gPSCIConfig[];
+extern uint32_t gPSCIConfigNum;
+
+/* * \brief Driver object */
+static PSCI_DrvObj gPSCIDrvObj =
+{
+    .lock = NULL,
+};
+
+/* ========================================================================== */
+/*                       API Function Definitions                             */
+/* ========================================================================== */
+
+void PSCI_init(void)
+{
+    PSCI_Handle handle;
+    uint32_t i;
+
+    /* Call init function for each config */
+    for(i = 0; i < gPSCIConfigNum; i++)
+    {
+        handle = &gPSCIConfig[i];
+        /* Input parameter validation */
+        if (handle->object != NULL)
+        {
+            /* Mark the object as available */
+            handle->object->isOpen = (bool)false;
+        }
+    }
+    /* Create driver lock */
+    (void)SemaphoreP_constructMutex(&gPSCIDrvObj.lockObj);
+    gPSCIDrvObj.lock = &gPSCIDrvObj.lockObj;
+}
+
+void PSCI_deinit(void)
+{
+    /* Delete driver lock */
+    if(NULL != gPSCIDrvObj.lock)
+    {
+        SemaphoreP_destruct(&gPSCIDrvObj.lockObj);
+        gPSCIDrvObj.lock = NULL;
+    }
+}
+
+PSCI_Handle PSCI_open(uint32_t idx)
+{
+    PSCI_Handle          handle = NULL;
+    PSCI_Object          *object = NULL;
+    int32_t              status = SystemP_SUCCESS;
+    uint32_t psciVer;
+
+    /* Check index */
+    if(idx >= gPSCIConfigNum)
+    {
+        status = SystemP_FAILURE;
+    }
+    else
+    {
+        handle = ( PSCI_Handle)&(gPSCIConfig[idx]);
+    }
+
+    DebugP_assert(NULL != gPSCIDrvObj.lock);
+    (void)SemaphoreP_pend(&gPSCIDrvObj.lockObj, SystemP_WAIT_FOREVER);
+
+    if(status == SystemP_SUCCESS)
+    {
+        object = ( PSCI_Object*)handle->object;
+        DebugP_assert(NULL != object);
+        if(object->isOpen)
+        {
+            /* Handle already opened */
+            status = SystemP_FAILURE;
+            handle = NULL;
+        }
+    }
+
+    if (status == SystemP_SUCCESS)
+    {
+        /* Mark the handle as being used */
+        object->isOpen = (bool)true;
+
+        /* Create object lock */
+        status = SemaphoreP_constructMutex(&object->mutex);
+
+        if (status == SystemP_SUCCESS)
+        {
+            /* Get PSCI version for sanity */
+            status = PSCI_getPSCIVersion(handle, &psciVer);
+
+            /* Print PSCI version. */
+            DebugP_log("PSCI Major version %d\r\n", PSCI_MAJOR_VERSION(psciVer));
+            DebugP_log("PSCI Minor version %d\r\n", PSCI_MINOR_VERSION(psciVer));
+        }
+    }
+
+    /* Close the driver object if initialization does not go through */
+    if(status != SystemP_SUCCESS)
+    {
+        PSCI_close(handle);
+    }
+
+    SemaphoreP_post(&gPSCIDrvObj.lockObj);
+    return (handle);
+}
+
+void PSCI_close(PSCI_Handle handle)
+{
+    PSCI_Object          *object = NULL;
+
+    if(handle != NULL)
+    {
+        object = ( PSCI_Object *)handle->object;
+        DebugP_assert(NULL != object);
+
+        DebugP_assert(NULL != gPSCIDrvObj.lock);
+        (void)SemaphoreP_pend(&gPSCIDrvObj.lockObj, SystemP_WAIT_FOREVER);
+
+        /* Destruct the instance lock */
+        (void)SemaphoreP_destruct(&object->mutex);
+
+        SemaphoreP_post(&gPSCIDrvObj.lockObj);
+    }
+}
+
+int32_t PSCI_getPSCIVersion(PSCI_Handle handle, uint32_t *psciVer)
+{
+    PSCI_Object *object = NULL;
+    int32_t status = SystemP_SUCCESS;
+
+    if((handle != NULL) && (psciVer != NULL))
+    {
+        object = (PSCI_Object*)handle->object;
+
+        /* Acquire the lock for this particular PSCI handle */
+        (void)SemaphoreP_pend(&object->mutex, SystemP_WAIT_FOREVER);
+
+        *psciVer = PSCI_function(PSCI_FUNC_PSCI_VERSION, 0, 0, 0);
+
+        /* Release the lock for this particular PSCI handle */
+        (void)SemaphoreP_post(&object->mutex);
+    }
+    else
+    {
+        status = SystemP_FAILURE;
+    }
+
+    return status;
+}
+
+int32_t PSCI_cpuOff(PSCI_Handle handle)
+{
+    PSCI_Object *object = NULL;
+    int32_t status = SystemP_SUCCESS;
+    int32_t err;
+
+    if(handle != NULL)
+    {
+        object = (PSCI_Object*)handle->object;
+
+        /* Acquire the lock for this particular PSCI handle */
+        (void)SemaphoreP_pend(&object->mutex, SystemP_WAIT_FOREVER);
+
+        err = PSCI_function(PSCI_FUNC_CPU_OFF, 0, 0, 0);
+
+        /* We should never reach here as the CPU should be turned off before this point.*/
+        status = PSCI_handleErr(err);
+
+        /* Release the lock for this particular PSCI handle */
+        (void)SemaphoreP_post(&object->mutex);
+    }
+    else
+    {
+        status = SystemP_FAILURE;
+    }
+
+	return status;
+}
+
+int32_t PSCI_cpuOn(PSCI_Handle handle, uint32_t cpuId, uint32_t entryPoint)
+{
+    PSCI_Object *object = NULL;
+    int32_t status = SystemP_SUCCESS;
+    int32_t err;
+
+    if(handle != NULL)
+    {
+        object = (PSCI_Object*)handle->object;
+
+        /* Acquire the lock for this particular PSCI handle */
+        (void)SemaphoreP_pend(&object->mutex, SystemP_WAIT_FOREVER);
+
+        err = PSCI_function(PSCI_FUNC_CPU_ON_64BIT, cpuId, entryPoint, 0);
+
+        status = PSCI_handleErr(err);
+
+        /* Release the lock for this particular PSCI handle */
+        (void)SemaphoreP_post(&object->mutex);
+    }
+    else
+    {
+        status = SystemP_FAILURE;
+    }
+
+    return status;
+}
+
+int32_t PSCI_systemReset(PSCI_Handle handle)
+{
+    PSCI_Object *object = NULL;
+    int32_t status = SystemP_SUCCESS;
+    int32_t err;
+
+    if(handle != NULL)
+    {
+        object = (PSCI_Object*)handle->object;
+
+        /* Acquire the lock for this particular PSCI handle */
+        (void)SemaphoreP_pend(&object->mutex, SystemP_WAIT_FOREVER);
+
+        err = PSCI_function(PSCI_FUNC_SYSTEM_RESET, 0, 0, 0);
+
+        /* We should never reach here as the system reset should have happened before this point.*/
+        status = PSCI_handleErr(err);
+
+        /* Release the lock for this particular PSCI handle */
+        (void)SemaphoreP_post(&object->mutex);
+    }
+    else
+    {
+        status = SystemP_FAILURE;
+    }
+
+    return status;
+}
+
+int32_t PSCI_getFeatures(PSCI_Handle handle, uint32_t functionId, uint32_t *feature)
+{
+    PSCI_Object *object = NULL;
+    int32_t status = SystemP_SUCCESS;
+
+    if((handle != NULL) && (feature != NULL))
+    {
+        object = (PSCI_Object*)handle->object;
+
+        /* Acquire the lock for this particular PSCI handle */
+        (void)SemaphoreP_pend(&object->mutex, SystemP_WAIT_FOREVER);
+
+        *feature = PSCI_function(PSCI_FUNC_PSCI_FEATURES, functionId, 0, 0);
+
+        /* Release the lock for this particular PSCI handle */
+        (void)SemaphoreP_post(&object->mutex);
+    }
+    else
+    {
+        status = SystemP_FAILURE;
+    }
+
+    return status;
+}
+
+/* ========================================================================== */
+/*                     API ISR Function Definitions                           */
+/* ========================================================================== */
+
+/* None */
+
+/* ========================================================================== */
+/*                      Internal Function Definitions                         */
+/* ========================================================================== */
+
+static inline uint32_t PSCI_function(uint32_t functionId, uint32_t arg0, uint32_t arg1, uint32_t arg2)
+{
+	SMCCC_Res res;
+
+	smccc_smc(functionId, arg0, arg1, arg2, 0, 0, 0, 0, &res);
+	return (uint32_t) res.a0;
+}
+
+static inline int32_t PSCI_handleErr(int32_t errNo)
+{
+    int32_t err;
+
+    switch (errNo) {
+	case PSCI_SUCCESS:
+		err = SystemP_SUCCESS;
+        break;
+	case PSCI_NOT_SUPPORTED:
+	case PSCI_INVALID_PARAMS:
+    case PSCI_DENIED:
+    case PSCI_ALREADY_ON:
+    case PSCI_ON_PENDING:
+    case PSCI_INTERNAL_FAILURE:
+    case PSCI_NOT_PRESENT:
+    case PSCI_DISABLED:
+	case PSCI_INVALID_ADDRESS:
+        err = SystemP_FAILURE;
+        break;
+    default:
+		err = SystemP_FAILURE;
+        break;
+    }
+
+    return err;
+}
---
+++ source/drivers/scp/psci/v0/psci.h
@@ -0,0 +1,230 @@
+/*
+ *  Copyright (C) 2025 Texas Instruments Incorporated
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *    Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ *    Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the
+ *    distribution.
+ *
+ *    Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+/**
+ *  \defgroup DRV_PSCI_MODULE APIs for PSCI
+ *  \ingroup DRV_MODULE
+ *
+ *  This module contains APIs to program and use the PSCI module.
+ *
+ *  @{
+ */
+
+/**
+ *  \file v0/psci.h
+ *
+ *  \brief PSCI Driver API/interface file.
+ */
+
+#ifndef PSCI_H_
+#define PSCI_H_
+
+/* ========================================================================== */
+/*                             Include Files                                  */
+/* ========================================================================== */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdio.h>
+#include <kernel/dpl/SemaphoreP.h>
+
+/* ========================================================================== */
+/*                           Macros & Typedefs                                */
+/* ========================================================================== */
+
+/* PSCI function interface */
+#define PSCI_BASE_FUNC		        (uint32_t) 0x84000000U
+#define PSCI_FUNC(n)				(PSCI_BASE_FUNC + (n))
+#define PSCI_FUNC_OFF_64BIT		    (uint32_t) 0x40000000U
+#define PSCI_BASE_FUNC_64BIT			\
+					(PSCI_BASE_FUNC + PSCI_FUNC_OFF_64BIT)
+#define PSCI_FUNC_64BIT(n)			(PSCI_BASE_FUNC_64BIT + (n))
+
+#define PSCI_FUNC_PSCI_VERSION		PSCI_FUNC(0)
+#define PSCI_FUNC_CPU_OFF			PSCI_FUNC(2)
+#define PSCI_FUNC_SYSTEM_RESET		PSCI_FUNC(9)
+#define PSCI_FUNC_CPU_ON_64BIT		PSCI_FUNC_64BIT(3)
+#define PSCI_FUNC_PSCI_FEATURES		PSCI_FUNC(10)
+
+/* Decode PSCI versions into Major and Minor Versions */
+#define PSCI_MAJOR_VERSION_SHIFT		(16U)
+#define PSCI_MINOR_VERSION_MASK			\
+		((1U << PSCI_MAJOR_VERSION_SHIFT) - 1)
+#define PSCI_MAJOR_VERSION_MASK			~PSCI_MINOR_VERSION_MASK
+#define PSCI_MAJOR_VERSION(ver)			\
+		(((ver) & PSCI_MAJOR_VERSION_MASK) >> PSCI_MAJOR_VERSION_SHIFT)
+#define PSCI_MINOR_VERSION(ver)			\
+		((ver) & PSCI_MINOR_VERSION_MASK)
+
+/* PSCI return values */
+#define PSCI_SUCCESS			    (0)
+#define PSCI_NOT_SUPPORTED			(-1)
+#define PSCI_INVALID_PARAMS			(-2)
+#define PSCI_DENIED				    (-3)
+#define PSCI_ALREADY_ON			    (-4)
+#define PSCI_ON_PENDING			    (-5)
+#define PSCI_INTERNAL_FAILURE		(-6)
+#define PSCI_NOT_PRESENT			(-7)
+#define PSCI_DISABLED			    (-8)
+#define PSCI_INVALID_ADDRESS		(-9)
+
+/* ========================================================================== */
+/*                          Structure Declarations                            */
+/* ========================================================================== */
+
+/**
+ *  \brief A handle that is returned from a PSCI_open() call.
+ */
+typedef struct PSCI_Config_s     *PSCI_Handle;
+
+/**
+ *  \brief  PSCI driver object
+ */
+typedef struct  PSCI_Object_s
+{
+/** Grants exclusive access to this PSCI Instance */
+    SemaphoreP_Object           mutex;
+/** Flag to indicate whether the instance is opened already */
+    bool                        isOpen;
+
+}  PSCI_Object;
+
+/**
+ *  \brief  PSCI Global Configuration
+ *
+ *  The  PSCI_Config structure contains a set of pointers used to characterise
+ *  the  PSCI driver implementation.
+ *
+ *  This structure needs to be defined before calling  PSCI_init() and it must
+ *  not be changed thereafter.
+ */
+typedef struct  PSCI_Config_s
+{
+/** Pointer to a driver specific data object */
+    PSCI_Object                      *object;
+
+}  PSCI_Config;
+
+/* ========================================================================== */
+/*                          Function Declarations                             */
+/* ========================================================================== */
+
+/**
+ * \brief Initialize the PSCI module.
+ */
+void PSCI_init(void);
+
+/**
+ * \brief Deinitialize the PSCI module.
+ */
+void PSCI_deinit(void);
+
+/**
+ * \brief Open the PSCI at index idx.
+ *
+ * \param idx [IN] Index of PSCI to open in global config
+ *
+ * \return PSCI_Handle
+ */
+PSCI_Handle PSCI_open(uint32_t idx);
+
+/**
+ * \brief Function to close the PSCI instance specified by the handle
+ *
+ * \pre #PSCI_open() has to be called first
+ *
+ * \param handle [IN] #PSCI_Handle returned from PSCI_open()
+ *
+ * \sa #PSCI_open()
+ */
+void PSCI_close(PSCI_Handle handle);
+
+/**
+ * \brief Get the PSCI version
+ *
+ * \param handle [IN] #PSCI_Handle returned from PSCI_open()
+ * \param psciVer [OUT] Pointer to PSCI version.
+ *
+ * \return PSCI_StatusCode
+ */
+int32_t PSCI_getPSCIVersion(PSCI_Handle handle, uint32_t *psciVer);
+
+/**
+ * \brief Power off the calling CPU
+ *
+ * \param handle [IN] #PSCI_Handle returned from PSCI_open()
+ *
+ * \return SystemP_FAILURE if failure, else should never return.
+ */
+int32_t PSCI_cpuOff(PSCI_Handle handle);
+
+/**
+ * \brief Power on the CPU required.
+ *
+ * \param handle [IN] #PSCI_Handle returned from PSCI_open()
+ * \param cpuId [IN] CPU ID of the CPU required to be powered on.
+ * \param entryPoint [IN] Entry Point of the CPU in the non-secure world.
+ *
+ * \return PSCI_StatusCode
+ */
+int32_t PSCI_cpuOn(PSCI_Handle handle, uint32_t cpuId, uint32_t entryPoint);
+
+/**
+ * \brief Cold reset of the entire system
+ *
+ * \param handle [IN] #PSCI_Handle returned from PSCI_open()
+ *
+ * \return SystemP_FAILURE if failure, else should never return.
+ */
+int32_t PSCI_systemReset(PSCI_Handle handle);
+
+/**
+ * \brief Get the features of a PSCI function.
+ *
+ * \param handle [IN] #PSCI_Handle returned from PSCI_open()
+ * \param functionId [IN] PSCI Function ID whose features are needed.
+ * \param feature [OUT] Pointer to the PSCI feature result.
+ *
+ * \return PSCI_StatusCode
+ */
+int32_t PSCI_getFeatures(PSCI_Handle handle, uint32_t functionId, uint32_t *feature);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* #ifndef PSCI_H_ */
+
+/** @} */

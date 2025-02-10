14b6ae854ef5e82735df4d66a2000c2d44ecc5a6
Vaibhav Reddy <vaibhav.reddy@ti.com>
2025-02-10 14:20:28 +0530

    am62lx: coremark-pro benchmark: Add compress jpeg worload application
     -Add c-jpeg workload which implements jpeg compression algorithm
    
     -The JPEG compression benchmark takes an image and encodes it to produce
      a compressed image
    
     -Add portbale layer (coremark_portable.c) for uart logging
      and timer dependencies
    
     -Add build infra to support the c-jpeg workload
    
    Fixes: SITSW-6311
    
    Signed-off-by: Vaibhav Reddy <vaibhav.reddy@ti.com>

--- .project/device/project_am62lx.js
+++ .project/device/project_am62lx.js
@@ -23,6 +23,7 @@ const device_defines = {
 };
 
 const example_file_list = [
+    "examples/benchmarks/coremark_pro_benchmark/cjpeg-rose7-preset/.project/project.js",
     "examples/benchmarks/dhrystone_benchmark/.project/project.js",
     "examples/benchmarks/interrupt_latency_benchmark/.project/project.js",
     "examples/drivers/ecap/ecap_apwm_mode/.project/project.js",

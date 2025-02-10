f96c359b8e7bcecdfd81ca8d6f2a235481dd25c5
Vaibhav Reddy <vaibhav.reddy@ti.com>
2025-02-10 14:53:49 +0530

    am62lx: coremark-pro benchmark: Add zip test workload application
     -Add zip test workload application which does zip compression
      using zlib library
    
     -Add build infra and update auto-gen files
    
    Fixes: SITSW-6311
    
    Signed-off-by: Vaibhav Reddy <vaibhav.reddy@ti.com>

--- .project/device/project_am62lx.js
+++ .project/device/project_am62lx.js
@@ -31,6 +31,7 @@ const example_file_list = [
     "examples/benchmarks/coremark_pro_benchmark/parser-125k/.project/project.js",
     "examples/benchmarks/coremark_pro_benchmark/radix2-big-64k/.project/project.js",
     "examples/benchmarks/coremark_pro_benchmark/sha-test/.project/project.js",
+    "examples/benchmarks/coremark_pro_benchmark/zip-test/.project/project.js",
     "examples/benchmarks/dhrystone_benchmark/.project/project.js",
     "examples/benchmarks/interrupt_latency_benchmark/.project/project.js",
     "examples/drivers/ecap/ecap_apwm_mode/.project/project.js",

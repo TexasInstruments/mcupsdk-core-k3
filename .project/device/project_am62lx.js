3a293c61a2116cd17f3180bbcd236c6062aa6544
Vaibhav Reddy <vaibhav.reddy@ti.com>
2025-02-11 18:07:59 +0530

    am62lx: coremark-pro benchmark: Add core workload application
     -Add core workload benchmarking application which is focused on
      processsing basic data structures
    
     -Add build infra to support the core benchmark
    
     -Add the auto generated files and syscfg files
    
    Fixes: SITSW-6311
    
    Signed-off-by: Vaibhav Reddy <vaibhav.reddy@ti.com>

--- .project/device/project_am62lx.js
+++ .project/device/project_am62lx.js
@@ -24,6 +24,7 @@ const device_defines = {
 
 const example_file_list = [
     "examples/benchmarks/coremark_pro_benchmark/cjpeg-rose7-preset/.project/project.js",
+    'examples/benchmarks/coremark_pro_benchmark/core/.project/project.js',
     "examples/benchmarks/dhrystone_benchmark/.project/project.js",
     "examples/benchmarks/interrupt_latency_benchmark/.project/project.js",
     "examples/drivers/ecap/ecap_apwm_mode/.project/project.js",

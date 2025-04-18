8ab7d96ba6159c80a63bb710ce8ce86ed96d73d6
Venkatesan Krishnamoorthy <v-krishnamoorthy@ti.com>
2025-04-18 13:42:55 +0530

    all: cpp: update hello_world cpp to use classes
     - Update hello_world cpp to use a base class and derived class
     - Also a global object to trigger global constructors
    
    Fixes: SITSW-7078
    
    Signed-off-by: Venkatesan Krishnamoorthy <v-krishnamoorthy@ti.com>

--- examples/hello_world_cpp/hello_world.cpp
+++ examples/hello_world_cpp/hello_world.cpp
@@ -37,8 +37,37 @@
 #include "ti_drivers_open_close.h"
 #include "ti_board_open_close.h"
 
+class IPrintMessage
+{
+    virtual void print() = 0;
+protected:
+    char* msg;
+public:
+    void Print()
+    {
+        print();
+    }
+};
+
+class HelloWorld : public IPrintMessage
+{
+    void print() 
+    { 
+        DebugP_log(msg);
+    }
+
+public:
+    HelloWorld()
+    {
+        static char hello[] = "Hello World!\r\n";
+        msg = hello;
+    }; 
+};
+
+HelloWorld obj; 
+
 void hello_world_main(void *args)
 {
-    DebugP_log("Hello World!\r\n");
+    obj.Print();
 }
 

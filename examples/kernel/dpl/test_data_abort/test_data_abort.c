bc969c36438c2e86bfda09399584a3b3f7944e71
Bhanu Prakash Chowdary Guttha <bhanu_prakash@ti.com>
2025-04-25 14:23:32 +0530

    am275x: kernal: Add examples for R5 exceptions
    -Add examples for debug aid for r5
     prefetch/data abort and undefined exceptions.
    
    Fixes: SITSW-7052
    
    Signed-off-by: Bhanu Prakash Chowdary Guttha <bhanu_prakash@ti.com>

---
+++ examples/kernel/dpl/test_data_abort/test_data_abort.c
@@ -0,0 +1,90 @@
+/*
+ *  Copyright (C) 2025 Texas Instruments Incorporated
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *    Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ *    Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the
+ *    distribution.
+ *
+ *    Neither the name of Texas Instruments Incorporated nor the names of
+ *    its contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+#include <stdio.h>
+#include "ti_drivers_config.h"
+#include "ti_drivers_open_close.h"
+#include "ti_board_open_close.h"
+#include <kernel/dpl/HwiP.h>
+
+
+#define RESTRICTED_ADDRESS 0xE0000000
+#define VALUE 0xFFFFFFFF
+
+uint32_t gDataAbortAddress = 0U;
+
+/**
+ * @brief  Provides information about the data abort exception
+ * 
+ * Contents of DFSR(Data Fault Status Register) register
+ *       1. status: indicates the type of fault generated
+ *       2. sd: distinguishes between an AXI Decode or Slave error on an external abort. This bit is only valid for external aborts. For all other aborts types of abort, this bit is set to zero
+ *       3. rw:  Indicates whether a read or write access caused an abort (0=read abort; 1=write abort)
+ *
+ * Contents of ADFSR(Auxillary Data Fault Status Register) register
+ *      1. index: returns the index value for the access giving the error
+ *      2. side_ext: value returned in this field indicates the source of the error
+ *      3. recoverable_error:  value returned in this field indicates if the error is recoverable (0=Unrecoverable error, 1=Recoverable Error)
+ *      4. cacheway: value returned in this field indicates the cache way or ways in which the error occurred
+ * 
+ * Please refer to R5F TRM for more information
+ * @param dfsr DFSR(Data Fault Status Register) register
+ * @param adfsr ADFSR(Auxillary Data Fault Status Register) register
+ * @param dfar Data Fault Address Register, address of the data trying to be accessed
+ * @param address Instruction causing the exception
+ * @param spsr Saved Program status registers to get the program status when the exception occured.
+ */
+void HwiP_user_data_abort_handler_c(DFSR dfsr, ADFSR adfsr, volatile uint32_t dfar, volatile uint32_t lr, 
+                                    volatile uint32_t spsr)
+{
+    gDataAbortAddress = dfar;
+
+    (void)dfsr;
+    (void)adfsr;
+    (void)dfar;
+    (void)lr;
+    (void)spsr;
+}
+
+void data_abort_main(void *args)
+{
+    /*Create a pointer to a restricted address and try to write*/
+    uint32_t *ptr=(uint32_t*)(RESTRICTED_ADDRESS);
+    *ptr=VALUE;
+
+    if(gDataAbortAddress == RESTRICTED_ADDRESS)
+    {
+        DebugP_log("All tests have passed!!\r\n");
+    }
+}
+

a6a324909f0900ad1b9fc26ca2b08d4cd237a879
Parthiv Sen <p-sen@ti.com>
2025-06-13 20:59:30 +0530

    am62dx : freertos smp : kernel testcase disable preemption
    -Implemented the testcase Disable Preemption where it
    is tested that a task is not preempted when it has
    disabled preemption for itself.
    
    Fixes: SITSW-7385
    
    Signed-off-by: Parthiv Sen <p-sen@ti.com>

--- .project/device/project_am62dx.js
+++ .project/device/project_am62dx.js
@@ -30,6 +30,7 @@ const component_file_list = [
     "source/networking/tsn/.project/project_tsn_unibase.js",
     "test/unity/.project/project.js",
     "test/kernel/smp_tests/disable_multiple_priorities/configs/.project/project.js",
+    "test/kernel/smp_tests/disable_preemption/configs/.project/project.js",
     "docs_src/docs/api_guide/doxy_samples/.project/project.js",
 ];
 
@@ -212,6 +213,7 @@ const example_file_list = [
 	"test/kernel/freertos_smp/.project/project.js",
 	"test/kernel/rov/.project/project.js",
     "test/kernel/smp_tests/disable_multiple_priorities/.project/project.js",
+    "test/kernel/smp_tests/disable_preemption/.project/project.js",
 	"test/kernel/test_smp_kernel/.project/project.js",
     "test/sdl/dcc/dcc_func_test/.project/project.js",
     "test/sdl/dcc/dcc_unit_test/.project/project.js",

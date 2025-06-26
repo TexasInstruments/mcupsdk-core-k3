e3c0192d12ba2e96073fd29a55223d24f94806ef
Parthiv Sen <p-sen@ti.com>
2025-06-26 14:34:15 +0530

    am62dx : freertos-smp : kernel testcase Schedule Equal Priority
    - Implemented kernel testcase Schedule Equal Priority which tests
    that the scheduler shcedules tasks of equal priorities at the
    same time in a round robin fashion.
    
    Fixes: SITSW-7385
    
    Signed-off-by: Parthiv Sen <p-sen@ti.com>

--- .project/device/project_am62dx.js
+++ .project/device/project_am62dx.js
@@ -35,6 +35,7 @@ const component_file_list = [
     "test/kernel/smp_tests/only_one_task_enter_critical/configs/.project/project.js",
     "test/kernel/smp_tests/only_one_task_enter_suspendall/configs/.project/project.js",
     "test/kernel/smp_tests/schedule_affinity/configs/.project/project.js",
+    "test/kernel/smp_tests/schedule_equal_priority/configs/.project/project.js",
     "docs_src/docs/api_guide/doxy_samples/.project/project.js",
 ];
 
@@ -224,6 +225,7 @@ const example_file_list = [
     "test/kernel/smp_tests/only_one_task_enter_critical/.project/project.js",
     "test/kernel/smp_tests/only_one_task_enter_suspendall/.project/project.js",
     "test/kernel/smp_tests/schedule_affinity/.project/project.js",
+    "test/kernel/smp_tests/schedule_equal_priority/.project/project.js",
 	"test/kernel/test_smp_kernel/.project/project.js",
     "test/sdl/dcc/dcc_func_test/.project/project.js",
     "test/sdl/dcc/dcc_unit_test/.project/project.js",

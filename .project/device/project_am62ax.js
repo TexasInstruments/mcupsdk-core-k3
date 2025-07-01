ff6dcf4a2a47a8404c8b3d245c110584ec1582ac
Parthiv Sen <p-sen@ti.com>
2025-07-01 19:13:48 +0530

    am62ax : freertos-smp : kernel testcase Schedule Affinity
    - Implemented kernel testcase Schedule Affinity which tests
    that the scheduler does not schedule a task that is pinned
    to a specific core.
    
    Fixes: SITSW-7385
    
    Signed-off-by: Parthiv Sen <p-sen@ti.com>

--- .project/device/project_am62ax.js
+++ .project/device/project_am62ax.js
@@ -29,6 +29,7 @@ const component_file_list_common = [
     "test/kernel/smp_tests/interrupt_wait_critical/configs/.project/project.js",
     "test/kernel/smp_tests/only_one_task_enter_critical/configs/.project/project.js",
     "test/kernel/smp_tests/only_one_task_enter_suspendall/configs/.project/project.js",
+    "test/kernel/smp_tests/schedule_affinity/configs/.project/project.js",
     "docs_src/docs/api_guide/doxy_samples/.project/project.js",
 ];
 
@@ -198,6 +199,7 @@ const example_file_list = [
     "test/kernel/smp_tests/interrupt_wait_critical/.project/project.js",
     "test/kernel/smp_tests/only_one_task_enter_critical/.project/project.js",
     "test/kernel/smp_tests/only_one_task_enter_suspendall/.project/project.js",
+    "test/kernel/smp_tests/schedule_affinity/.project/project.js",
     "test/security/crypto/test_sa3ul_aes/.project/project.js",
     "test/security/crypto/test_sa3ul_rng/.project/project.js",
     "test/security/crypto/test_sa3ul_sha/.project/project.js",

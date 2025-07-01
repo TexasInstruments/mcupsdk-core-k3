dd00f5754883cda58839bf0c922029b2e9569ad9
Parthiv Sen <p-sen@ti.com>
2025-07-01 19:05:13 +0530

    am62ax : freertos-smp : kernel testcase Only One Task Enter Suspend All
    -Implemented the kernel testcase Only One Task Enter Suspend All
    which tests that only one task can enter the secion protected by
    vTaskSuspendAll/xTaskResumeAll at a time.
    
    Fixes: SITSW-7385
    
    Signed-off-by: Parthv Sen <p-sen@ti.com>

--- .project/device/project_am62ax.js
+++ .project/device/project_am62ax.js
@@ -28,6 +28,7 @@ const component_file_list_common = [
     "test/kernel/smp_tests/disable_preemption/configs/.project/project.js",
     "test/kernel/smp_tests/interrupt_wait_critical/configs/.project/project.js",
     "test/kernel/smp_tests/only_one_task_enter_critical/configs/.project/project.js",
+    "test/kernel/smp_tests/only_one_task_enter_suspendall/configs/.project/project.js",
     "docs_src/docs/api_guide/doxy_samples/.project/project.js",
 ];
 
@@ -196,6 +197,7 @@ const example_file_list = [
     "test/kernel/smp_tests/disable_preemption/.project/project.js",
     "test/kernel/smp_tests/interrupt_wait_critical/.project/project.js",
     "test/kernel/smp_tests/only_one_task_enter_critical/.project/project.js",
+    "test/kernel/smp_tests/only_one_task_enter_suspendall/.project/project.js",
     "test/security/crypto/test_sa3ul_aes/.project/project.js",
     "test/security/crypto/test_sa3ul_rng/.project/project.js",
     "test/security/crypto/test_sa3ul_sha/.project/project.js",

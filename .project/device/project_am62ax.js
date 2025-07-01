1f02ed7b942594d29c0f01e236096ecafb772c70
Parthiv Sen <p-sen@ti.com>
2025-07-01 19:34:50 +0530

    am62ax : freertos-smp : kernel testcase Schedule Highest Priority
    - Implemented kernel testcase Schedule Highest Priority which tests
    that the scheduler schedules the highest priority ready tasks.
    
    Fixes: SITSW-7385
    
    Signed-off-by: Parthiv Sen <p-sen@ti.com>

--- .project/device/project_am62ax.js
+++ .project/device/project_am62ax.js
@@ -31,6 +31,7 @@ const component_file_list_common = [
     "test/kernel/smp_tests/only_one_task_enter_suspendall/configs/.project/project.js",
     "test/kernel/smp_tests/schedule_affinity/configs/.project/project.js",
     "test/kernel/smp_tests/schedule_equal_priority/configs/.project/project.js",
+    "test/kernel/smp_tests/schedule_highest_priority/configs/.project/project.js",
     "docs_src/docs/api_guide/doxy_samples/.project/project.js",
 ];
 
@@ -202,6 +203,7 @@ const example_file_list = [
     "test/kernel/smp_tests/only_one_task_enter_suspendall/.project/project.js",
     "test/kernel/smp_tests/schedule_affinity/.project/project.js",
     "test/kernel/smp_tests/schedule_equal_priority/.project/project.js",
+    "test/kernel/smp_tests/schedule_highest_priority/.project/project.js",
     "test/security/crypto/test_sa3ul_aes/.project/project.js",
     "test/security/crypto/test_sa3ul_rng/.project/project.js",
     "test/security/crypto/test_sa3ul_sha/.project/project.js",

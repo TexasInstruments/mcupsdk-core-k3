0a83cf9b7516ad21e9158a423e018a956908da57
Anoop <anoop.rk@ti.com>
2025-07-05 17:19:58 +0530

    am62dx: mcasp: Udpate start stop test for 62d
        * Update mcasp start stop test to use codecs
          in AM62Dx
    
    Fixes: SITSW-7638
    
    Signed-off-by: Anoop <anoop.rk@ti.com>

--- test/drivers/mcasp/mcasp_start_stop_test/mcasp_start_stop.c
+++ test/drivers/mcasp/mcasp_start_stop_test/mcasp_start_stop.c
@@ -49,7 +49,7 @@
 /* Audio buffer settings */
 #define APP_MCASP_AUDIO_BUFF_SIZE   (48000U * 5U * 8U)
 
-#if defined (SOC_AM62AX) || defined (SOC_AM62X) || defined (SOC_AM62DX) || defined (SOC_AM275X)
+#if defined (SOC_AM62AX) || defined (SOC_AM62X)
 /* AM62Ax CODEC I2C address */
 #define APP_MCASP_CODEC_ADDR    (0x1BU)
 
@@ -58,7 +58,7 @@
 
 /* Codec reset pin for I/O expander */
 #define IO_EXP_CODEC_RESET_PIN  (0x8U)
-#endif
+
 
 /***************************** Codec Register address *************************/
 #define AIC31_PAGE_SEL_REG                  (0x0U)
@@ -80,6 +80,22 @@
 #define AIC_DAC_R1_HPROUT_VOL_CTRL_REG      (0x40U)
 #define AIC_HPROUT_LEVEL_CTRL_REG           (0x41U)
 
+#endif /* SOC_AM62AX || SOC_AM62X */
+
+#if defined (SOC_AM62DX)
+
+typedef struct {
+    uint8_t reg;
+    uint8_t val;
+} Adc_RegCfg;
+
+typedef struct {
+    uint8_t reg;
+    uint8_t val;
+} Dac_RegCfg;
+
+#endif /* SOC_AM62DX */
+
 /* ========================================================================== */
 /*                           Global Variables                                 */
 /* ========================================================================== */
@@ -96,9 +112,86 @@ static SemaphoreP_Object gTxSem;
 
 uint8_t gMcaspRxBuffer[APP_MCASP_AUDIO_BUFF_SIZE] __attribute__((aligned(256)));
 
+#if defined (SOC_AM62DX)
+
+Adc_RegCfg gPcm6240RegInst0[] =
+{
+    /* Page select 0 */
+    {0x00, 0x00},
+    /* Reset */
+    {0x01, 0x01},
+    /* Page select 0 */
+    {0x00, 0x00},
+    /* Disable sleep */
+    {0x02, 0x09},
+    /* High impedance for unused cycles */
+    {0x07, 0x31},
+    /* CH1 at slot 0 */
+    {0x0B, 0x00},
+    /* CH2 at slot 1 */
+    {0x0C, 0x01},
+    /* Set micbias to 5v */
+    {0x3b, 0x70},
+    /* CH1 Microphone input */
+    {0x3c, 0x10},
+    /* CH2 Microphone input */
+    {0x41, 0x10},
+    /* Input channel enable */
+    {0x74, 0xC0},
+    /* Power up micbias and power up all ADC channels */
+    {0x75, 0xE0},
+};
+
+Adc_RegCfg gPcm6240RegInst1[] =
+{
+    /* Page select 0 */
+    {0x00, 0x00},
+    /* Reset */
+    {0x01, 0x01},
+    /* Page select 0 */
+    {0x00, 0x00},
+    /* Disable sleep */
+    {0x02, 0x09},
+    /* High impedance for unused cycles */
+    {0x07, 0x31},
+    /* Input channel disable */
+    {0x74, 0x00},
+};
+
+Dac_RegCfg gTad5212Reg[] =
+{
+    /* Page select 0 */
+    {0x00, 0x00},
+    /* Disable sleep */
+    {0x02, 0x01},
+    /* I2S, 32b mode */
+    {0x1A, 0x70},
+    /* CH1 - Input from DAC, Mono single ended output at OUT1P */
+    {0x64, 0x28},
+    /* CH1 - Line Out driver with 300ohm impedance, 0dB gain */
+    {0x65, 0x20},
+    /* CH1 - 0dB gain */
+    {0x67, 0xC9},
+    /* CH2 - Input from DAC, Mono single ended output at OUT2P */
+    {0x6B, 0x28},
+    /* CH2 - Line Out driver with 300ohm impedance, 0dB gain */
+    {0x6C, 0x20},
+    /* CH2 - 0dB gain */
+    {0x6E, 0xC9},
+    /* PASI channel2 input is right slot0 */
+    {0x29, 0x30},
+    /* Enable output CH1 CH2 */
+    {0x76, 0x0C},
+    {0x78, 0x40},
+};
+
+#endif /* SOC_AM62DX */
+
 /* ========================================================================== */
 /*                        Static Function Declaration                         */
 /* ========================================================================== */
+
+#if defined (SOC_AM62AX) || defined (SOC_AM62X)
 /* Reset codec */
 static void mcasp_codec_reset(void);
 /* Configure codec TLV320AIC31 */
@@ -106,6 +199,16 @@ static void mcasp_aic31_codec_config(void);
 /* I2C register write for Codec */
 static void I2C_writeReg(I2C_Handle handle, uint8_t devAddr, uint8_t reg,
                                     uint8_t val);
+#endif /* SOC_AM62AX || SOC_AM62X */
+
+#if defined (SOC_AM62DX)
+/* Configure PCM6240 */
+static int32_t Board_adcConfig(I2C_Handle handle, uint8_t devAddr, uint32_t instNum);
+/* Configure TAD5212 */
+static int32_t Board_dacConfig(I2C_Handle handle, uint8_t devAddr);
+/* Cofigure codec */
+static int32_t Board_codecConfig(void);
+#endif /* SOC_AM62DX */
 
 void mcasp_start_stop_main(void *args)
 {
@@ -113,7 +216,11 @@ void mcasp_start_stop_main(void *args)
     MCASP_Handle    mcaspHandle;
     char            valueChar;
 
+#if defined (SOC_AM62AX) || defined (SOC_AM62X)
     mcasp_aic31_codec_config();
+#elif defined (SOC_AM62DX)
+    Board_codecConfig();
+#endif
 
     DebugP_log("[MCASP] Audio stop restart example started.\r\n");
 
@@ -226,6 +333,7 @@ void mcasp_start_stop_main(void *args)
 
 }
 
+#if defined (SOC_AM62AX) || defined (SOC_AM62X)
 static void I2C_writeReg(I2C_Handle handle, uint8_t devAddr, uint8_t reg,
                                     uint8_t val)
 {
@@ -353,6 +461,7 @@ static void mcasp_aic31_codec_config(void)
         I2C_writeReg(i2cHandle, deviceAddress, AIC_RDAC_VOL_CTRL, 0x00);
     }
 }
+#endif /* SOC_AM62AX || SOC_AM62X */
 
 void mcasp_txcb(MCASP_Handle handle,
                           MCASP_Transaction *transaction)
@@ -365,3 +474,123 @@ void mcasp_rxcb(MCASP_Handle handle,
 {
     SemaphoreP_post(&gRxSem);
 }
+
+
+#if defined (SOC_AM62DX)
+
+static int32_t Board_codecConfig(void)
+{
+    int32_t status = SystemP_SUCCESS;
+
+    /* Configure TAD5212 instances */
+    {
+        I2C_Handle      i2cHandle;
+        i2cHandle = gI2cHandle[CONFIG_I2C0];
+
+        status = Board_dacConfig(i2cHandle, 0x50);
+        DebugP_assert(status == SystemP_SUCCESS);
+    }
+
+    /* Configure PCM6240 instances */
+    {
+        I2C_Handle      i2cHandle;
+        i2cHandle = gI2cHandle[CONFIG_I2C0];
+
+        status = Board_adcConfig(i2cHandle, 0x48, 0);
+        DebugP_assert(status == SystemP_SUCCESS);
+
+        /* Disable second instance and give Hi-Z in unused cycles */
+        status = Board_adcConfig(i2cHandle, 0x49, 1);
+        DebugP_assert(status == SystemP_SUCCESS);
+    }
+
+    return status;
+}
+
+static int32_t Board_dacConfig(I2C_Handle handle, uint8_t devAddr)
+{
+    int32_t status = SystemP_SUCCESS;
+    uint32_t count = sizeof(gTad5212Reg)/sizeof(Adc_RegCfg);
+    uint32_t i = 0;
+    I2C_Transaction i2cTransaction;
+    uint8_t txBuffer[2];
+
+    status = I2C_probe(handle, devAddr);
+    DebugP_assert(status == SystemP_SUCCESS);
+
+    ClockP_usleep(100);
+
+    for(i = 0; i < count; i++)
+    {
+        I2C_Transaction_init(&i2cTransaction);
+        i2cTransaction.writeBuf   = txBuffer;
+        i2cTransaction.writeCount = 2;
+        i2cTransaction.targetAddress = devAddr;
+        txBuffer[0] = gTad5212Reg[i].reg;
+        txBuffer[1] = gTad5212Reg[i].val;
+        status = I2C_transfer(handle, &i2cTransaction);
+
+        if(status != SystemP_SUCCESS)
+        {
+            break;
+        }
+
+        ClockP_usleep(100);
+    }
+
+    return status;
+}
+
+static int32_t Board_adcConfig(I2C_Handle handle, uint8_t devAddr, uint32_t inst)
+{
+    int32_t status = SystemP_SUCCESS;
+    uint32_t count = 0;
+    uint32_t i = 0;
+    I2C_Transaction i2cTransaction;
+    uint8_t txBuffer[2];
+
+    status = I2C_probe(handle, devAddr);
+    DebugP_assert(status == SystemP_SUCCESS);
+
+    ClockP_usleep(200);
+
+    if(inst == 0)
+    {
+        count = sizeof(gPcm6240RegInst0)/sizeof(gPcm6240RegInst0[0]);
+    }
+    else if(inst == 1)
+    {
+        count = sizeof(gPcm6240RegInst1)/sizeof(gPcm6240RegInst1[0]);
+    }
+
+    for(i = 0; i < count; i++)
+    {
+        I2C_Transaction_init(&i2cTransaction);
+        i2cTransaction.writeBuf   = txBuffer;
+        i2cTransaction.writeCount = 2;
+        i2cTransaction.targetAddress = devAddr;
+        if(inst == 0)
+        {
+            txBuffer[0] = gPcm6240RegInst0[i].reg;
+            txBuffer[1] = gPcm6240RegInst0[i].val;
+        }
+        else if(inst == 1)
+        {
+            txBuffer[0] = gPcm6240RegInst1[i].reg;
+            txBuffer[1] = gPcm6240RegInst1[i].val;
+        }
+
+        status = I2C_transfer(handle, &i2cTransaction);
+
+        if(status != SystemP_SUCCESS)
+        {
+            break;
+        }
+
+        ClockP_usleep(200);
+    }
+
+    return status;
+}
+
+#endif /* SOC_AM62DX  */

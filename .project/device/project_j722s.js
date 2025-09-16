858192426c11e469ac18f2ea47066703ad76ef1f
Pratap Reddy <pratap.reddy@ti.com>
2025-09-16 06:26:47 +0530

    j722s: SBL: Enable sbl uart support
     - Enabled SBL UART boot support for J722S.
     - Updated image load address in app makefile template which is required
       for proper execution of the application images from sbl uart.
     - Regenerated the example/test makefiles with updated image load address
    
     Fixes: PDK-18774
    
    Signed-off-by: Pratap Reddy <pratap.reddy@ti.com>

--- .project/device/project_j722s.js
+++ .project/device/project_j722s.js
@@ -40,6 +40,7 @@ const example_file_list = [
     "examples/drivers/boot/sbl_ospi_multistage/sbl_ospi_stage2/.project/project.js",
     "examples/drivers/boot/sbl_sd_multistage/sbl_sd_stage1/.project/project.js",
     "examples/drivers/boot/sbl_sd_multistage/sbl_sd_stage2/.project/project.js",
+    "examples/drivers/boot/sbl_uart/.project/project.js",
     "examples/drivers/boot/sbl_uart_uniflash/.project/project.js",
     "examples/drivers/csirx/csirx_capture_testapp/.project/project.js",
     "examples/drivers/csirx/csirxtx_loopback_test/.project/project.js",

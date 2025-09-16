c890c2d77f5141738e7971e401c40b92cc6b0cc4
Anoop <anoop.rk@ti.com>
2025-09-16 13:12:37 +0530

    am275x: mcasp: Add mcasp aux clock select test
        * Add mcasp aux clock selection test case
          with mcasp in loopback mode with a non
          default aux clock source
    
    Fixes: SITSW-7908
    
    Signed-off-by: Anoop <anoop.rk@ti.com>

---
+++ test/drivers/mcasp/mcasp_auxclk_sel/.project/project_am275x.js
@@ -0,0 +1,113 @@
+let path = require('path');
+
+let device = "am275x";
+
+const files = {
+	common: [
+		"mcasp_auxclk_sel.c",
+		"main.c",
+	],
+};
+
+/* Relative to where the makefile will be generated
+ * Typically at <example_folder>/<BOARD>/<core_os_combo>/<compiler>
+ */
+const filedirs = {
+	common: [
+		"..",       /* core_os_combo base */
+		"../../..", /* Example base */
+	],
+};
+
+const libdirs_freertos_c75 = {
+	common: [
+		"${MCU_PLUS_SDK_PATH}/source/kernel/freertos/lib",
+		"${MCU_PLUS_SDK_PATH}/source/drivers/lib",
+        "${MCU_PLUS_SDK_PATH}/source/board/lib",
+		"${MCU_PLUS_SDK_PATH}/source/drivers/udma/lib",
+	],
+};
+
+const includes_freertos_c75 = {
+    common: [
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/FreeRTOS-Kernel/include",
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/portable/TI_CGT/DSP_C75X",
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/config/am275x/c75x",
+    ],
+};
+
+const libs_freertos_c75 = {
+    common: [
+        "freertos.am275x.c75x.ti-c7000.${ConfigName}.lib",
+        "board.am275x.c75x.ti-c7000.${ConfigName}.lib",
+        "drivers.am275x.c75x.ti-c7000.${ConfigName}.lib",
+        "udma.am275x.c75x.ti-c7000.${ConfigName}.lib",
+    ],
+};
+
+const lnkfiles = {
+	common: [
+		"linker.cmd",
+	]
+};
+
+const syscfgfile = "../example.syscfg";
+
+const templates_freertos_c75 =
+[
+    {
+        input: ".project/templates/am275x/common/linker_c75ss0.cmd.xdt",
+        output: "linker.cmd",
+    },
+    {
+        input: ".project/templates/am275x/freertos/main_freertos.c.xdt",
+        output: "../main.c",
+        options: {
+            entryFunction: "mcasp_auxclk_sel_main",
+            stackSize: 64*1024,
+        },
+    }
+];
+
+const buildOptionCombos = [
+    { device: device, cpu: "c75ss0-0", cgt: "ti-c7000",    board: "am275x-evm", os: "freertos"},
+];
+
+function getComponentProperty() {
+    let property = {};
+
+    property.dirPath = path.resolve(__dirname, "..");
+    property.type = "executable";
+    property.name = "mcasp_auxclk_sel";
+    property.isInternal = true;
+    property.tirexResourceSubClass = [ "example.gettingstarted" ];
+    property.description = "This example validates non default MCASP AUX clk select option"
+    property.buildOptionCombos = buildOptionCombos;
+
+    return property;
+}
+
+function getComponentBuildProperty(buildOption) {
+    let build_property = {};
+
+    build_property.files = files;
+    build_property.filedirs = filedirs;
+    build_property.lnkfiles = lnkfiles;
+    build_property.syscfgfile = syscfgfile;
+
+    if(buildOption.cpu.match(/c75*/)) {
+        if(buildOption.os.match(/freertos*/)) {
+            build_property.includes = includes_freertos_c75;
+            build_property.libdirs = libdirs_freertos_c75;
+            build_property.libs = libs_freertos_c75;
+            build_property.templates = templates_freertos_c75;
+        }
+    }
+
+    return build_property;
+}
+
+module.exports = {
+    getComponentProperty,
+    getComponentBuildProperty,
+};

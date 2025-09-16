b11095792f4efeb101ebcad5fe8e2ab954606c1b
Anoop <anoop.rk@ti.com>
2025-09-16 17:23:57 +0530

    am62lx: mcasp: Add mcasp aux clock select test
        * Add mcasp aux clock selection test case
          with mcasp in loopback mode with a non
          default aux clock source
    
    Fixes: SITSW-7908
    
    Signed-off-by: Anoop <anoop.rk@ti.com>

---
+++ test/drivers/mcasp/mcasp_auxclk_sel/.project/project_am62lx.js
@@ -0,0 +1,116 @@
+let path = require('path');
+
+let device = "am62lx";
+
+const files = {
+	common: [
+		"mcasp_auxclk_sel.c",
+		"main.c",
+	],
+};
+
+/* Relative to where the makefile will be generated
+ * Typically at <example_folder>/<BOARD>/<core_os_combo>/<compiler>
+ */
+const filedirs = {
+	common: [
+		"..",       /* core_os_combo base */
+		"../../..", /* Example base */
+	],
+};
+
+const libdirs_freertos_a53 = {
+	common: [
+		"${MCU_PLUS_SDK_PATH}/source/kernel/freertos/lib",
+		"${MCU_PLUS_SDK_PATH}/source/drivers/lib",
+	],
+};
+
+const includes_freertos_a53 = {
+    common: [
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/FreeRTOS-Kernel/include",
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/portable/GCC/ARM_CA53",
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/config/am62lx/a53",
+    ],
+};
+
+const libs_freertos_a53 = {
+    common: [
+        "freertos.am62lx.a53.gcc-aarch64.${ConfigName}.lib",
+        "drivers.am62lx.a53.gcc-aarch64.${ConfigName}.lib",
+    ],
+};
+
+const lnkfiles = {
+	common: [
+		"linker.cmd",
+	]
+};
+
+const defines_common = {
+    common:[
+        "SOC_AM62LX",
+    ]
+};
+
+const syscfgfile = "../example.syscfg";
+
+const templates_freertos_a53 =
+[
+    {
+        input: ".project/templates/am62lx/common/linker_a53.cmd.xdt",
+        output: "linker.cmd",
+    },
+    {
+        input: ".project/templates/am62lx/freertos/main_freertos.c.xdt",
+        output: "../main.c",
+        options: {
+            entryFunction: "mcasp_auxclk_sel_main",
+        },
+    }
+];
+
+const buildOptionCombos = [
+    { device: device, cpu: "a53ss0-0", cgt: "gcc-aarch64", board: "am62lx-evm", os: "freertos"}
+];
+
+function getComponentProperty() {
+    let property = {};
+
+    property.dirPath = path.resolve(__dirname, "..");
+    property.type = "executable";
+    property.name = "mcasp_auxclk_sel";
+    property.isInternal = true;
+    property.tirexResourceSubClass = [ "example.gettingstarted" ];
+    property.description = "This example validates non default MCASP AUX clk select option"
+    property.buildOptionCombos = buildOptionCombos;
+
+    return property;
+}
+
+function getComponentBuildProperty(buildOption) {
+    let build_property = {};
+
+    build_property.files = files;
+    build_property.filedirs = filedirs;
+    build_property.lnkfiles = lnkfiles;
+    build_property.syscfgfile = syscfgfile;
+    build_property.defines = defines_common;
+
+    if(buildOption.cpu.match(/a53*/)) {
+        if(buildOption.os.match(/freertos*/) )
+        {
+            build_property.includes = includes_freertos_a53;
+            build_property.libdirs = libdirs_freertos_a53;
+            build_property.libs = libs_freertos_a53;
+            build_property.templates = templates_freertos_a53;
+        }
+    }
+
+    return build_property;
+}
+
+module.exports = {
+    getComponentProperty,
+    getComponentBuildProperty,
+};

73bc29475274475e487f2e3944273b185f8bcad7
srirama <srirama@ti.com>
2025-09-18 20:01:05 +0530

    am62px: SOC: Fix Misra C Rule 4.3 issue
    MISRA.ASM.ENCAPS
    
    Assembly language shall be encapsulated and isolated.
    
    Fixes: SITSW-7018
    
    Signed-off-by: Kusu Sri Rama Govinda Rao <srirama@ti.com>

--- source/drivers/soc/am62px/soc.c
+++ source/drivers/soc/am62px/soc.c
@@ -49,6 +49,13 @@
 /* PSC (Power Sleep Controller) Domain enable */
 #define PSC_MODSTATE_ENABLE         (0x3U)
 
+static inline void SOC_executeWfi(void)
+{
+#if defined(__ARM_ARCH_7R__)
+    __asm__ __volatile__ ("wfi"   "\n\t": : : "memory");
+#endif
+}
+
 int32_t SOC_moduleClockEnable(uint32_t moduleId, uint32_t enable)
 {
     int32_t status = SystemP_SUCCESS;
@@ -615,9 +622,7 @@ void SOC_generateSwWarmResetMainDomain(void)
     SOC_controlModuleLockMMR(SOC_DOMAIN_ID_MAIN, rstPartition);
 
     /* execute wfi */
-#if defined(__ARM_ARCH_7R__)
-    __asm__ __volatile__ ("wfi"   "\n\t": : : "memory");
-#endif
+    SOC_executeWfi();
 }
 
 void SOC_generateSwPORResetMainDomain(void)
@@ -637,9 +642,7 @@ void SOC_generateSwPORResetMainDomain(void)
     SOC_controlModuleLockMMR(SOC_DOMAIN_ID_MAIN, rstPartition);
 
     /* execute wfi */
-#if defined(__ARM_ARCH_7R__)
-    __asm__ __volatile__ ("wfi"   "\n\t": : : "memory");
-#endif
+    SOC_executeWfi();
 }
 
 uint32_t SOC_getWarmResetCauseMainDomain(void)

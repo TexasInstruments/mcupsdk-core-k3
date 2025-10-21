6288e16b424bc118644a0676619be02339ad3665
Dillon Frank <d-frank@ti.com>
2025-10-21 09:55:33 -0500

    allsoc: dm: Fix MISRA.ETYPE.CATEGORY.DIFFERENT.2012
    The operands of the operator '<' do not have the
    same essential type category: one is 'signed' and
    the other is 'unsigned'
    
    Fixes: SYSFW-8650
    
    Signed-off-by: Dillon Frank <d-frank@ti.com>

--- source/drivers/device_manager/priv/trace/trace-buffer.c
+++ source/drivers/device_manager/priv/trace/trace-buffer.c
@@ -78,9 +78,9 @@ static uint8_t *logbuf_pos;
 
 int32_t trace_print_buffer_string(const uint8_t *str)
 {
-    int32_t i = 0, ret = CSL_PASS;
+    int32_t i = 0U, ret = CSL_PASS;
 
-    for (i = 0; i < TRACE_PRINT_MAX_LENGTH; i++) {
+    for (i = 0U; i < TRACE_PRINT_MAX_LENGTH; i++) {
         if (str[i] != 0U) {
             ret = trace_print_buffer(str[i]);
         }

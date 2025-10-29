17dc6697c0b429df055e05b96b66e2127aa33820
Yash Srivastava <y-srivastava@ti.com>
2025-10-29 14:33:08 +0530

    j722s: makefile: Add -Og flag for debug builds.
    - Add -Og optimization flag in the source driver library makefiles in
      debug profile to provide restricted optimizations while preserving
      code debuggability.
    
    - The above is to accomodate for the increase in code size arrising
      due to MMCSD changes.
    
    Fixes: SITSW-8608
    
    Signed-off-by: Yash Srivastava <y-srivastava@ti.com>

--- source/drivers/.project/project_j722s.js
+++ source/drivers/.project/project_j722s.js
@@ -537,6 +537,12 @@ const defines_main_r5 = {
     ],
 };
 
+const cflags_wkup_r5f = {
+    debug: [
+        "-Og",
+    ]
+}
+
 const defines_c75ss0 = {
     common: [
     ],
@@ -574,6 +580,7 @@ function getComponentBuildProperty(buildOption) {
         build_property.files = files_wkup_r5f;
         build_property.defines = defines_wkup_r5;
         build_property.filedirs = filedirs_wkup_r5f;
+        build_property.cflags = cflags_wkup_r5f;
     }
     else if(buildOption.cpu.match(/mcu-r5f*/)) {
         build_property.files = files_mcu_r5f;

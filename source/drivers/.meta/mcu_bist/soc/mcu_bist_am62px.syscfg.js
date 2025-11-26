bf4e30ccd545c6819b605ceaabbc2edfcc79b864
raj <r-panganamala@ti.com>
2025-11-26 15:12:14 +0530

    am62px,am62x: sdl: Add syscfg support for other working PBIST instances
    -Enable instances whose PBIST tests are passing.
    -Ensured backward compatibility with existing MCU PBIST implementation.
    -Update argument name in SDL_PBIST_startAllTests  to runPosTest.
    
    Fixes: PROC_SDL-9399
    
    Signed-off-by: Raj Panganamala <r-panganamala@ti.com>

--- source/drivers/.meta/mcu_bist/soc/mcu_bist_am62px.syscfg.js
+++ source/drivers/.meta/mcu_bist/soc/mcu_bist_am62px.syscfg.js
@@ -6,6 +6,8 @@ function getSdlPbistInstances()
 {
     return [
         "SDL_PBIST_INST_MCU",
+        "SDL_PBIST_INST_MPU",
+        "SDL_PBIST_INST_ENCODE"
     ]
 }
 function getSdlPbistAuxDevices()
@@ -14,6 +16,18 @@ function getSdlPbistAuxDevices()
         [
             "TISCI_DEV_MCU_PBIST0",
         ],
+        [
+            "TISCI_DEV_COMPUTE_CLUSTER0",
+            "TISCI_DEV_A53SS0_CORE_0",
+            "TISCI_DEV_A53SS0_CORE_1",
+            "TISCI_DEV_A53SS0_CORE_2",
+            "TISCI_DEV_A53SS0_CORE_3",
+            "TISCI_DEV_COMPUTE_CLUSTER0_PBIST_0",
+        ],
+        [
+            "TISCI_DEV_CODEC0",
+            "TISCI_DEV_PBIST3"
+        ]
     ]
 }
 exports = {
--- source/drivers/.meta/mcu_bist/soc/mcu_bist_am62x.syscfg.js
+++ source/drivers/.meta/mcu_bist/soc/mcu_bist_am62x.syscfg.js
@@ -6,6 +6,7 @@ function getSdlPbistInstances()
 {
     return [
         "SDL_PBIST_INST_MCU",
+        "SDL_PBIST_INST_MPU"
     ]
 }
 function getSdlPbistAuxDevices()
@@ -13,6 +14,14 @@ function getSdlPbistAuxDevices()
     return [
         [
         ],
+        [
+            "TISCI_DEV_COMPUTE_CLUSTER0",
+            "TISCI_DEV_A53SS0_CORE_0",
+            "TISCI_DEV_A53SS0_CORE_1",
+            "TISCI_DEV_A53SS0_CORE_2",
+            "TISCI_DEV_A53SS0_CORE_3",
+            "TISCI_DEV_COMPUTE_CLUSTER0_PBIST_0",
+        ]
     ]
 }
 exports = {

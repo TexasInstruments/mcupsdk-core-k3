ff64e47c664737d21b7ab2775ff7c2438b1d6e16
raj <r-panganamala@ti.com>
2025-12-18 16:01:01 +0530

    all: sdl: Update PBIST non-blocking self-test API.
    -Earlier, the API supports only positive PBIST tests.
    -This change enables support for both positive and negative tests.
    -Source files are updated to align with the modified API
    
    Fixes: PROC_SDL-9613
    
    Signed-off-by: Raj Panganamala <r-panganamala@ti.com>

--- source/sdl/sdl_pbist.h
+++ source/sdl/sdl_pbist.h
@@ -135,6 +135,7 @@ int32_t SDL_PBIST_selfTest(SDL_PBIST_inst instance, SDL_PBIST_testType testType,
  *  defined in the soc-specific header file.
  *
  *  \param instance         [IN]  PBIST instance
+ *  \param testType         [IN]  PBIST test type to run.
  *
  *  \return The SDL error code for the API.
  *                                 If pResult is NULL: SDL_EBADARGS
@@ -142,7 +143,7 @@ int32_t SDL_PBIST_selfTest(SDL_PBIST_inst instance, SDL_PBIST_testType testType,
  *                                 If other error happened: SDL_EFAIL
  *                                 Success: SDL_PASS
  */
-int32_t SDL_SBL_PBIST_selfTest(SDL_PBIST_inst instance);
+int32_t SDL_SBL_PBIST_selfTest(SDL_PBIST_inst instance, SDL_PBIST_testType testType);
 
 /**
  *  \brief PBIST API to check if the test is complete.

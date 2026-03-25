999bba613c9a8efe19416f7c7a2419e8e819281a
Prasad Konnur <p-konnur@ti.com>
2026-03-25 17:30:03 +0530

    j722s: build: c7x compiler flags
     - Sync c7x compiler flags with jacinto_master.
     - Add 'DBUILD_C7X' flag required for UDMA driver.
     - Fix UDMA examples on c7x cores.
    
    Fixes: PDK-20136, PDK-20136
    
    Signed-off-by: Prasad Konnur <p-konnur@ti.com>

--- .project/cgt/cgt_ti-c7000.js
+++ .project/cgt/cgt_ti-c7000.js
@@ -75,7 +75,7 @@ const cgt_common = {
     },
 };
 
-const cgt_common_am275x_j722s = {
+const cgt_common_am275x = {
     path: "CGT_TI_C7000_PATH",
     cc: "cl7x",
     ar: "ar7x",
@@ -149,9 +149,80 @@ const cgt_common_am275x_j722s = {
 };
 
 const cgt_common_j722s = {
+    path: "CGT_TI_C7000_PATH",
+    cc: "cl7x",
+    ar: "ar7x",
+    lnk: "cl7x",
+    strip: "strip7x",
+    objcopy: "",
+
+    includes: {
+        common: [
+            "${CG_TOOL_ROOT}/include",
+            "${MCU_PLUS_SDK_PATH}/source"
+        ],
+    },
     cflags: {
         common: [
+            "-mv7524",
+            "--abi=eabi",
+            "-q",
+            "-mo",
+            "-pden",
+            "-pds=238",
+            "-pds=880",
+            "-pds1110",
+            "--endian=little",
+            "--disable_inlining",
+            "-ea.se71",
+            "--emit_warnings_as_errors",
+            "--diag_suppress=770", // to suppress pointer to small int conversion error
+            "--diag_suppress=69", // to suppress -1 to uint32t error
+            "--diag_suppress=70", // to suppress char pointer to uint32t error
+            "-DBUILD_C7X",
+            "-DBUILD_C7X_1",
             "--advice:performance=none", // to suppress optimization advice
+        ],
+        debug: [
+            "-Dxdc_target_name__=C71",
+            "-D_DEBUG_=1",
+            "--opt_level=0",
+            "--symdebug:dwarf",
+            "-DBUILD_C75X",
+        ],
+        release: [
+            "-Dxdc_target_name__=C71",
+            "--opt_level=3",
+            "--symdebug:none",
+            "-DBUILD_C75X",
+        ],
+    },
+    arflags: {
+        common: [
+            "rq",
+        ],
+    },
+    lflags: {
+        common: [
+            "--silicon_version=7524",
+            "-z",
+            "--emit_warnings_as_errors",
+            "--diag_suppress=10063",
+            "--ram_model",
+            "--warn_sections",
+            "-q",
+            "-x",
+            ""
+        ],
+    },
+    libdirs: {
+        common: [
+            "${CG_TOOL_ROOT}/lib",
+        ],
+    },
+    libs: {
+        common: [
+            "libc.a",
         ],
     },
 };
@@ -177,11 +248,11 @@ function getCgtOptions(cpu)
     let cgtOptions = {};
     if(buildOption.device=="am275x")
     {
-        cgtOptions = cgt_common_am275x_j722s;
+        cgtOptions = cgt_common_am275x;
     }
     else if(buildOption.device=="j722s")
     {
-        cgtOptions = common.mergeCgtOptions(cgt_common_am275x_j722s, cgt_common_j722s);
+        cgtOptions = cgt_common_j722s;
         if(cpu.match(/c75ss0-0*/))
         {
             cgtOptions = common.mergeCgtOptions(cgtOptions, cgt_common_c75ss0);

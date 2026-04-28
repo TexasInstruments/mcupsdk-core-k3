84911ff7aedd3f943c7f4c0704298e953b5d6e5a
Bhanu Prakash Chowdary Guttha <bhanu_prakash@ti.com>
2026-04-28 12:07:30 +0530

    am275x: optishare: Add optishare demo
    -Add optishare demo with ipc notify echo
    
    Fixes: SITSW-9150
    
    Signed-off-by: Bhanu Prakash Chowdary Guttha <bhanu_prakash@ti.com>

--- .project/device/project_am275x.js
+++ .project/device/project_am275x.js
@@ -100,6 +100,7 @@ const example_file_list = [
     "examples/drivers/i2c/i2c_memory_read/.project/project.js",
     "examples/drivers/i2c/i2c_temperature/.project/project.js",
     "examples/drivers/ipc/ipc_notify_echo/.project/project.js",
+    "examples/drivers/ipc/ipc_notify_echo_optishare/.project/project_am275x.js",
     "examples/drivers/ipc/ipc_rpmsg_echo/.project/project.js",
     "examples/drivers/ipc/ipc_rpmsg_rtos_audio_filter_offload/.project/project.js",
     "examples/drivers/mcan/mcan_external_read_write/.project/project_am275x.js",
---
+++ examples/drivers/ipc/ipc_notify_echo_optishare/.project/project_am275x.js
@@ -0,0 +1,140 @@
+let path = require('path');
+
+let device = "am275x";
+
+const files = {
+    common: [
+        "ipc_notify_echo.c",
+        "main.c",
+    ],
+};
+
+/* Relative to where the makefile will be generated
+ * Typically at <example_folder>/<BOARD>/<core_os_combo>/<compiler>
+ */
+const filedirs = {
+    common: [
+        "..",       /* core_os_combo base */
+        "../../..", /* Example base */
+    ],
+};
+
+const libdirs_freertos = {
+    common: [
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/lib",
+        "${MCU_PLUS_SDK_PATH}/source/drivers/lib",
+    ],
+};
+
+const includes_freertos_r5f = {
+    common: [
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/FreeRTOS-Kernel/include",
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/portable/TI_ARM_CLANG/ARM_CR5F",
+        "${MCU_PLUS_SDK_PATH}/source/kernel/freertos/config/am275x/r5f",
+    ],
+};
+
+const libs_freertos_r5f = {
+    common: [
+        "freertos.am275x.r5f.ti-arm-clang.${ConfigName}.lib",
+        "drivers.am275x.r5f.ti-arm-clang.${ConfigName}.lib",
+    ],
+};
+
+const lnkfiles = {
+    common: [
+        "linker.cmd",
+    ]
+};
+
+const syscfgfile = "../example.syscfg";
+
+const readmeDoxygenPageTag = "EXAMPLES_DRIVERS_IPC_NOTIFY_ECHO";
+
+const templates_freertos_r5f =
+[
+    {
+        input: ".project/templates/am275x/common/linker_main-r5f.cmd.xdt",
+        output: "linker.cmd",
+        options: {
+            isOptishare: "true",
+        }
+    },
+    {
+        input: ".project/templates/am275x/freertos/main_freertos.c.xdt",
+        output: "../main.c",
+        options: {
+            entryFunction: "ipc_notify_echo_main",
+            isOptishare: "true",
+        },
+    }
+];
+
+const buildOptionCombos = [
+    { device: device, cpu: "r5fss0-0", cgt: "ti-arm-clang", board: "am275x-evm", os: "freertos", isPartOfSystemProject: true},
+    { device: device, cpu: "r5fss0-1", cgt: "ti-arm-clang", board: "am275x-evm", os: "freertos", isPartOfSystemProject: true},
+    { device: device, cpu: "r5fss1-0", cgt: "ti-arm-clang", board: "am275x-evm", os: "freertos", isPartOfSystemProject: true},
+    { device: device, cpu: "r5fss1-1", cgt: "ti-arm-clang", board: "am275x-evm", os: "freertos", isPartOfSystemProject: true},
+];
+
+const systemProjects =[
+    {
+        name: "ipc_notify_echo_optishare",
+        tag: "freertos",
+        skipProjectSpec: false,
+        readmeDoxygenPageTag: readmeDoxygenPageTag,
+        board: "am275x-evm",
+        optishare_memspecs : "optishare_memmap.json",
+        projects: [
+            { device: device, cpu: "r5fss0-0", cgt: "ti-arm-clang", board: "am275x-evm", os: "freertos" },
+            { device: device, cpu: "r5fss0-1", cgt: "ti-arm-clang", board: "am275x-evm", os: "freertos" },
+            { device: device, cpu: "r5fss1-0", cgt: "ti-arm-clang", board: "am275x-evm", os: "freertos" },
+            { device: device, cpu: "r5fss1-1", cgt: "ti-arm-clang", board: "am275x-evm", os: "freertos" },
+        ],
+    }
+]
+
+function getComponentProperty() {
+    let property = {};
+
+    property.dirPath = path.resolve(__dirname, "..");
+    property.type = "executable";
+    property.name = "ipc_notify_echo_optishare";
+    property.isInternal = false;
+    property.description ="A IPC Notify echo example"
+    property.buildOptionCombos = buildOptionCombos;
+    property.isLogSHM = true;
+
+    return property;
+}
+
+function getComponentBuildProperty(buildOption) {
+    let build_property = {};
+
+    build_property.files = files;
+    build_property.filedirs = filedirs;
+    build_property.lnkfiles = lnkfiles;
+    build_property.syscfgfile = syscfgfile;
+    build_property.readmeDoxygenPageTag = readmeDoxygenPageTag;
+    if(buildOption.cpu.match(/r5fss*/)) {
+        if(buildOption.os.match(/freertos*/) )
+        {
+            build_property.includes = includes_freertos_r5f;
+            build_property.libdirs = libdirs_freertos;
+            build_property.libs = libs_freertos_r5f;
+            build_property.templates = templates_freertos_r5f;
+        }
+    }
+    return build_property;
+}
+
+function getSystemProjects(device)
+{
+    return systemProjects;
+}
+
+module.exports = {
+    getComponentProperty,
+    getComponentBuildProperty,
+    getSystemProjects,
+};

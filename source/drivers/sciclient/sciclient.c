01d6a5a9a761e76a225d87ccfd8884e77af966f6
Sparsh Kumar <sparsh-kumar@ti.com>
2026-06-24 11:31:35 +0530

    allsocs: sciclient: Add missing secure‑message IDs to secure context
    The function Sciclient_getCurrentContext only
    listed a subset of the messages that must be
    handled in a secure context. Those omitted
    messages were therefore classified as non‑secure,
    which led to SYSFW returning NACK when those
    messages were called.
    
    Extend the if condition to include the remaining
    secure messages to fix this issue.
    
    Fixes: SITSW-10544
    
    Signed-off-by: Sparsh Kumar <sparsh-kumar@ti.com>

--- source/drivers/sciclient/sciclient.c
+++ source/drivers/sciclient/sciclient.c
@@ -1132,6 +1132,8 @@ uint32_t Sciclient_getCurrentContext(uint16_t messageType)
         case TISCI_MSG_DISABLE_JTAG_UNLOCK:
         case TISCI_MSG_DISABLE_JTAG_UNLOCK_CHECK:
         case TISCI_MSG_KEYRING_IMPORT:
+        case TISCI_MSG_ALLOW_FWL_CTRL_READ:
+        case TISCI_MSG_FORBID_FWL_CTRL_READ:
             retVal = Sciclient_handle.secureContextId;
             break;
 

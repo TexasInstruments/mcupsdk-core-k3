8f4e07c802fab5b029ca3ff2755cbac15c13bb5b
Prashant Shivhare <p-shivhare@ti.com>
2026-07-14 11:21:11 +0530

    am275x: test: Add ASTRA test template
    Add ASTRA test template for JTAG unlock example.
    
    Fixes: SITSW-11642
    
    Signed-off-by: Prashant Shivhare <p-shivhare@ti.com>

--- examples/security/runtime_debug_unlock/.project/project_am275x.js
+++ examples/security/runtime_debug_unlock/.project/project_am275x.js
@@ -73,6 +73,17 @@ const templates_freertos_r5f =
     }
 ];
 
+const robot_template = {
+    input: ".project/templates/am275x/astra/tests.robot.xdt",
+    output: "../tests.robot",
+    options: {
+        componentName: "Security",
+        testCaseName: "Security: Runtime debug unlock application",
+        testCaseIds: "SITSW-3816",
+        expectedString: "All tests have passed",
+    },
+};
+
 const buildOptionCombos = [
     { device: device, cpu: "r5fss0-0", cgt: "ti-arm-clang", board: "am275x-evm", os: "freertos"},
 ];
@@ -110,6 +121,7 @@ function getComponentBuildProperty(buildOption) {
         }
     }
 
+    build_property.templates = [...(build_property.templates || []), robot_template];
     return build_property;
 }
 

14592e8609bf86f214877d7e13a986190c69cdf5
Sparsh Kumar <sparsh-kumar@ti.com>
2026-07-28 17:37:02 +0530

    am62ax/px/dx: test: sciclient: Update dynamic analysis tests
    Add missing TISCI messages to the gTestMessageType
    array in SCIClient Dynamic Analysis test application.
    
    Fixes: SITSW-12603
    Signed-off-by: Sparsh Kumar <sparsh-kumar@ti.com>

--- test/drivers/sciclient/sciclient_dynamic_analysis/test_sciclient_ti_msg.c
+++ test/drivers/sciclient/sciclient_dynamic_analysis/test_sciclient_ti_msg.c
@@ -120,6 +120,8 @@ const int32_t gTestMessageType[] =
     TISCI_MSG_SA2UL_SET_DKEK,
     TISCI_MSG_SA2UL_GET_DKEK,
     TISCI_MSG_SA2UL_RELEASE_DKEK,
+    TISCI_MSG_GET_KEY_WRITER_TYPE,
+    TISCI_MSG_SET_KEY_WRITER_TYPE,    
     0xFFFF,  /* Negative test case */
 };
 
